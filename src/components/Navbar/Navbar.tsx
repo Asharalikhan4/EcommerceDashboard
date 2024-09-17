1806334f427d38edea61cab03e3ef15a29d8d201
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-09-17 13:42:33 +0530

    Basic Setup done

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Navbar: FC = () => {
+    return (
+        <div className="text-3xl font-bold underline">
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
